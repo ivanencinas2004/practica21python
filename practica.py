cc34e4d9659ac571ed9d658851ca5278d864b57c
ivanencinas2004 <117982973+ivanencinas2004@users.noreply.github.com>
2022-11-17 09:50:38 +0100

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,7 @@
+#• Diseña un algoritmo que permita mostrar las tablas de multiplicar del 1 al 10
+
+for x in range(11):
+    for y in range(11):
+        print(f'{x} * {y} es {x*y}')    
+
+        
